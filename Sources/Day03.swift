8cfd103d4e2534111aa27cc87083d4d4626121ad
Oscar Echeverri <oscar@echeverri.ca>
2024-12-03 17:16:00 -0500

    Day 3 Part 2 Monstrosity

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -2,30 +2,94 @@ import Foundation
 struct Day03: AdventDay {
     var data: String
 
-    private var _entities = [(Int,Int)]()
-    
+    private var _entities: String
+
+    enum Conditional {
+        case yes
+        case dont
+
+    }
+
     init(data: String) {
         self.data = data
-        let ranges = data.ranges(of: /mul\([0-9]{1,3},[0-9]{1,3}\)/)
-        for range in ranges {
-            let str = data[(data.index(range.lowerBound, offsetBy: 4))...(data.index(range.upperBound, offsetBy: -2))]
-            let nums = str.components(separatedBy: ",")
-            _entities.append((Int(nums[0])!, Int(nums[1])!))
-        }
-        
-        
+        self._entities = data
+
     }
-    var entities: [(Int,Int)] {
+    var entities: String {
         return _entities
     }
 
     func part1() -> Any {
-        entities.reduce(0) {
+        let ranges = data.ranges(of: /mul\([0-9]{1,3},[0-9]{1,3}\)/)
+        var entities = [(Int, Int)]()
+        for range in ranges {
+            let str = data[(data.index(range.lowerBound, offsetBy: 4))...(data.index(range.upperBound, offsetBy: -2))]
+            let nums = str.components(separatedBy: ",")
+            entities.append((Int(nums[0])!, Int(nums[1])!))
+        }
+
+        return entities.reduce(0) {
             $0 + $1.0 * $1.1
         }
     }
 
     func part2() -> Any {
-        0
+        var conditionalRanges = [(conditional: Conditional, range: Range<String.Index>)]()
+        let ranges = data.ranges(of: /mul\([0-9]{1,3},[0-9]{1,3}\)/)
+        let doRanges = data.ranges(of: /do\(\)/)
+        let dontRanges = data.ranges(of: /don't\(\)/)
+
+        for doRange in doRanges {
+            conditionalRanges.append((.yes, doRange))
+        }
+        for dontRange in dontRanges {
+            conditionalRanges.append((.dont, dontRange))
+        }
+
+        conditionalRanges.sort(by: { $0.range.lowerBound < $1.range.lowerBound })
+        var enabledRanges = [Range<String.Index>]()
+
+        var currentConditional = Conditional.yes
+        var lastLowerBount = data.startIndex
+
+        for i in 0..<conditionalRanges.count {
+            let range = conditionalRanges[i]
+            if currentConditional == range.conditional {
+                continue
+            }
+            if currentConditional == .yes {
+                enabledRanges.append(lastLowerBount..<range.range.lowerBound)
+                currentConditional = .dont
+            } else {
+                currentConditional = .yes
+            }
+            lastLowerBount = range.range.upperBound
+            if i == conditionalRanges.count - 1, currentConditional == .yes {
+                enabledRanges.append(lastLowerBount..<data.endIndex)
+            }
+        }
+
+        var entities = [(Int, Int)]()
+        func isInEnabled(range: Range<String.Index>) -> Bool {
+            for enabled in enabledRanges {
+                if enabled.contains(range.lowerBound) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        for range in ranges {
+            if isInEnabled(range: range) {
+                let str = data[(data.index(range.lowerBound, offsetBy: 4))...(data.index(range.upperBound, offsetBy: -2))]
+                let nums = str.components(separatedBy: ",")
+                entities.append((Int(nums[0])!, Int(nums[1])!))
+            }
+
+        }
+
+        return entities.reduce(0) {
+            $0 + $1.0 * $1.1
+        }
     }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -7,6 +7,7 @@ import Testing
 struct Day03Tests {
   // Smoke test data provided in the challenge question
     let testData = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    let dodontData = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
     @Test func testPart1() async throws {
       let challenge = Day03(data: testData)
@@ -14,7 +15,7 @@ struct Day03Tests {
     }
 
     @Test func testPart2() async throws {
-      let challenge = Day03(data: testData)
-      #expect(String(describing: challenge.part2()) == "32000")
+        let challenge = Day03(data: dodontData)
+      #expect(String(describing: challenge.part2()) == "48")
     }
 }
