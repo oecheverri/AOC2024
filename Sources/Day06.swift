99c20b6409c0e455d9a21254d71b7e926c374501
Oscar Echeverri <oscar@echeverri.ca>
2024-12-07 17:53:52 -0500

    Day06Part02

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -1,15 +1,81 @@
+import Foundation
+enum Direction: Character {
+    case up = "^"
+    case down = "v"
+    case left = "<"
+    case right = ">"
+    
+    var turnRight: Direction {
+        switch self {
+        case .up: return .right
+        case .right: return .down
+        case .down: return .left
+        case .left: return .up
+        }
+    }
+
+    var turnedLeft: Direction {
+        switch self {
+        case .up: return .left
+        case .left: return .down
+        case .down: return .right
+        case .right: return .up
+        }
+    }
+}
 
 struct Point: Equatable, Hashable {
     let x: Int
     let y: Int
+    
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    init (x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+
+    func nextPoint(facing direction: Direction) -> Point {
+        switch direction {
+        case .up:
+            .init(x: x, y: y - 1)
+        case .down:
+            .init(x: x, y: y + 1)
+        case .left:
+            .init(x: x - 1, y: y)
+        case .right:
+            .init(x: x + 1, y: y)
+        }
+    }
 }
+
+
+enum MovementError: Error, Equatable {
+    case Stuck(Point)
+}
+
+extension Array where Element == [Character] {
+    subscript(point: Point) -> Character {
+        get {
+            self[point.y][point.x]
+        }
+        set {
+            self[point.y][point.x] = newValue
+        }
+    }
+}
+
+
 
 struct Day06: AdventDay {
     
     var data: String
 
     let lab: [[Character]]
-    var startingPosition = Point(x: 0, y: 0)
+    var initialStartPosition = Point(x: 0, y: 0)
 
     init(data: String) {
         self.data = data
@@ -23,7 +89,7 @@ struct Day06: AdventDay {
         for y in 0..<lab.count {
             for (x, char) in lab[y].enumerated() {
                 if Direction(rawValue: char) != nil {
-                    self.startingPosition = Point(x: x, y: y)
+                    self.initialStartPosition = Point(x: x, y: y)
                     break
                 }
             }
@@ -33,22 +99,6 @@ struct Day06: AdventDay {
     var entities: [[Character]] {
         lab
     }
-
-    enum Direction: Character {
-        case up = "^"
-        case down = "v"
-        case left = "<"
-        case right = ">"
-        
-        var turnRight: Direction {
-            switch self {
-            case .up: return .right
-            case .right: return .down
-            case .down: return .left
-            case .left: return .up
-            }
-        }
-    }
     
     func isInBounds(_ position: Point) -> Bool {
         lab.indices.contains(position.y) && lab[position.y].indices.contains(position.x)
@@ -56,41 +106,145 @@ struct Day06: AdventDay {
     
     func part1() -> Any {
         
-        guard var currentDirection = Direction(rawValue: lab[startingPosition.y][startingPosition.x]) else {
+        guard var currentDirection = Direction(rawValue: lab[initialStartPosition.y][initialStartPosition.x]) else {
             return 0
         }
-        var currentPosition = self.startingPosition
+        var currentPosition = self.initialStartPosition
+        var nextPosition = currentPosition.nextPoint(facing: currentDirection)
         var visited: Set<Point> = [currentPosition]
-
-        while true {
-            let nextPos: Point
-            switch currentDirection {
-            case .up:
-                nextPos = Point(x: currentPosition.x, y: currentPosition.y - 1)
-            case .down:
-                nextPos = Point(x: currentPosition.x, y: currentPosition.y + 1)
-            case .left:
-                nextPos = Point(x: currentPosition.x - 1, y: currentPosition.y)
-            case .right:
-                nextPos = Point(x: currentPosition.x + 1, y: currentPosition.y)
-            }
+        while isInBounds(nextPosition) {
             
-            guard isInBounds(nextPos) else { return visited.count }
-            
-            if lab[nextPos.y][nextPos.x] == "#" {
+            guard lab[nextPosition] != "#", lab[nextPosition] != "O" else {
                 currentDirection = currentDirection.turnRight
-            } else {
-                currentPosition = nextPos
-                visited.insert(currentPosition)
+                nextPosition = currentPosition.nextPoint(facing: currentDirection)
+                continue
             }
+            currentPosition = nextPosition
+            nextPosition = currentPosition.nextPoint(facing: currentDirection)
+            visited.insert(currentPosition)
         }
         
         return visited.count
     }
 
-    func part2() -> Any {
-        0
+    func walk(
+        lab: [[Character]],
+        starting point: Point,
+        startingDirection: Direction,
+        shouldPlaceObstacles: Bool = true
+    ) throws -> Int {
+        var visited: [Point: Set<Direction>] = [:]
+        var placedObstacles: Set<Point> = []
+        var currentPosition = point
+        var currentDirection = startingDirection
+        var nextPosition = currentPosition.nextPoint(facing: currentDirection)
+
+        while isInBounds(nextPosition) {
+            
+            guard lab[nextPosition] != "#", lab[nextPosition] != "O" else {
+                currentDirection = currentDirection.turnRight
+                nextPosition = currentPosition.nextPoint(facing: currentDirection)
+                continue
+            }
+            
+            if shouldPlaceObstacles {
+                var modifiedLab = lab
+                modifiedLab[nextPosition] = "O"
+                do {
+                    _ = try walk(
+                        lab: modifiedLab,
+                        starting: initialStartPosition,
+                        startingDirection: .up,
+                        shouldPlaceObstacles: false
+                    )
+                } catch {
+                    placedObstacles.insert(nextPosition)
+                }
+            }
+            
+            if let arrivalDirections = visited[nextPosition],
+                arrivalDirections.contains(currentDirection) {
+                throw MovementError.Stuck(currentPosition)
+            }
+            
+            visited[nextPosition, default: []].insert(currentDirection)
+            currentPosition = nextPosition
+            nextPosition = currentPosition.nextPoint(facing: currentDirection)
+        }
+
+        return placedObstacles.count
     }
+    
+    func part2() -> Any {
+        guard let currentDirection = Direction(rawValue: lab[initialStartPosition.y][initialStartPosition.x]) else {
+            return 0
+        }
+        let result = try! walk(lab: lab, starting: initialStartPosition, startingDirection: currentDirection, shouldPlaceObstacles: true)
+        return result
+    }
+
+    func printLab(lab: [[Character]], _ visited: [Point: Set<Direction>], placedObstacles: Set<Point>, currentPosition: Point, currentDirection: Direction) {
+        
+        var logger = LogDestination()
+        
+        for (rIndex, row) in lab.enumerated() {
+            for (cIndex, cell) in row.enumerated() {
+                let point = Point(x: cIndex, y: rIndex)
+                if point == currentPosition {
+                    print(currentDirection.rawValue, terminator: ",", to: &logger)
+                } else if placedObstacles.contains(point) {
+                    print("O", terminator: ",", to: &logger)
+                } else if let directions = visited[point] {
+                    print(getTravelPath(directions: .init(directions)), terminator: ",", to: &logger)
+                } else {
+                    print(cell, terminator: ",", to: &logger)
+                }
+            }
+            print("", to: &logger)
+        }
+    }
+
+    func getTravelPath(directions: [Direction]) -> String {
+        directions.reduce("") {
+            switch $1 {
+            case .left, .right:
+                if $0 == "" {
+                    return "-"
+                } else
+                if $0 == "|" {
+                    return "+"
+                }
+            case .down, .up:
+                if $0 == "" {
+                    return "|"
+                } else
+                if $0 == "-" {
+                    return "+"
+                }
+            }
+            return $0
+        }
+    }
+
+}
+
+final class LogDestination: TextOutputStream {
+  private let path: String
+  init() {
+    let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+    path = paths.first! + "/log"
+      print("Logging to: \(path)")
+  }
+
+  func write(_ string: String) {
+    if let data = string.data(using: .utf8), let fileHandle = FileHandle(forWritingAtPath: path) {
+      defer {
+        fileHandle.closeFile()
+      }
+      fileHandle.seekToEndOfFile()
+      fileHandle.write(data)
+    }
+  }
 }
 
 
--- Tests/Day06.swift
+++ Tests/Day06.swift
@@ -26,6 +26,46 @@ struct Day06Tests {
 
     @Test func testPart2() async throws {
         let challenge = Day06(data: testData)
-        #expect(String(describing: challenge.part2()) == "32000")
+        #expect(String(describing: challenge.part2()) == "6")
+    }
+    
+    @Test(
+        "Test can place obstacles"
+        , arguments: zip([
+            Point(4, 6),
+            .init(6, 6),
+            .init(4, 8),
+            .init(2, 8),
+            .init(6, 7),
+            .init(7, 8)
+        ],
+        [
+            Direction.left,
+            .down,
+            .left,
+            .left,
+            .right,
+            .down
+        ])
+    )
+    
+    func testWalking(startingPosition: Point, startingDirection: Direction) throws {
+        let challenge = Day06(data: testData)
+        let insertedObstacle = startingPosition.nextPoint(facing: startingDirection)
+        var modified = challenge.entities
+        modified[insertedObstacle.y][insertedObstacle.x] = "#"
+        
+        #expect(
+            throws: MovementError.Stuck(startingPosition)
+        ) {
+            try challenge.walk(
+                lab: modified,
+                starting: startingPosition,
+                startingDirection: startingDirection,
+                shouldPlaceObstacles: false
+            )
+        }
+        
+        
     }
 }
