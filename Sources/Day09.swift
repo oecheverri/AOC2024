02168cbbdf768a07623b3f3177cdc474e4521674
Oscar Echeverri <oscar.echeverri@wbd.com>
2024-12-09 13:28:30 -0500

    Day 9 Part 2

--- Sources/Day09.swift
+++ Sources/Day09.swift
@@ -1,9 +1,53 @@
+protocol DiskContent: Sendable, Equatable {
+    var size: Int { get }
+    func merge(with other: any DiskContent) -> Self?
+}
+
+struct File: DiskContent {
+    let size: Int
+    let fileId: Int
+
+    func merge(with other: any DiskContent) -> Self? {
+        guard let otherFile = other as? File, otherFile.fileId == fileId else { return nil }
+        return File(size: size + otherFile.size, fileId: fileId)
+    }
+}
+
+struct Empty: DiskContent {
+    let size: Int
+    func merge(with other: any DiskContent) -> Self? {
+        guard let empty = other as? Empty else { return nil }
+        return .init(size: size + empty.size)
+    }
+
+    func reserve(blocks: Int) -> [Empty]? {
+        guard blocks <= size else { return nil }
+        if blocks == size {
+            return [self]
+        }
+        return [.init(size: blocks), .init(size: size-blocks)]
+    }
+}
+
+extension Array where Element == any DiskContent {
+    func flatten() -> [Int?] {
+        var flat = [Int?]()
+        for content in self {
+            guard let file = content as? File else {
+                flat.append(contentsOf: [Int?](repeating: nil, count: content.size))
+                continue
+            }
+            flat.append(contentsOf: [Int](repeating: file.fileId, count: file.size))
+        }
+        return flat
+    }
+}
 struct Day09: AdventDay {
 
     var data: String
 
-    var dataArray = [Int?]()
-    var entities: [Int?] {
+    var dataArray = [any DiskContent]()
+    var entities: [any DiskContent] {
         dataArray
     }
 
@@ -16,14 +60,14 @@ struct Day09: AdventDay {
         while left < self.data.endIndex {
             dataArray.append(
                 contentsOf: Array(
-                    repeating: fileId,
+                    repeating: File(size: 1, fileId: fileId),
                     count: Int(String(data[left]))!
                 )
             )
             if right < self.data.endIndex {
                 dataArray.append(
                     contentsOf: Array(
-                        repeating: nil,
+                        repeating: Empty(size: 1),
                         count: Int(String(data[right]))!
                     )
                 )
@@ -38,33 +82,84 @@ struct Day09: AdventDay {
 
     }
 
-    func compactedEntities() -> [Int] {
+    func compactedEntities() -> [any DiskContent] {
         var compactedEntities = entities
         var left = 0
         var right = compactedEntities.count-1
         while left < right {
-            while compactedEntities[left] != nil {
+            while compactedEntities[left] is File {
                 left+=1
-                if left == right { break }
             }
-            while compactedEntities[right] == nil {
+            while compactedEntities[right] is Empty {
                 right-=1
-                if left == right { break }
+            }
+            if right < left {
+                break
             }
             compactedEntities.swapAt(left, right)
         }
-
-        return compactedEntities.compactMap(\.self)
-
+        return compactedEntities
     }
+
+    func mergeBlocks() -> [any DiskContent] {
+        var mergedBlocks: [any DiskContent] = []
+
+        var i = 0
+        while i < dataArray.count {
+            var merged: any DiskContent = dataArray[i]
+            while i < dataArray.count-1 {
+                guard let tempMerge = merged.merge(with: dataArray[i+1]) else {
+                    break
+                }
+                merged = tempMerge
+                i+=1
+            }
+            mergedBlocks.append(merged)
+            i=i+1
+        }
+        return mergedBlocks
+    }
+
     func part1() -> Any {
         compactedEntities().enumerated()
             .reduce(0) {
-                return $0 + $1.offset * $1.element
+                guard let file = $1.element as? File else { return $0 }
+                return $0 + $1.offset * file.fileId
             }
     }
 
     func part2() -> Any {
-        0
+        var blocks = mergeBlocks()
+        var left = 0
+        var right = blocks.count - 1
+
+        while left < right {
+            guard let file = blocks[right] as? File else {
+                right-=1
+                continue
+            }
+
+            while left < right {
+                guard let empty = blocks[left] as? Empty,
+                        let split = empty.reserve(blocks: file.size)
+                else {
+                    left+=1
+                    continue
+                }
+                blocks.remove(at: left)
+                blocks.insert(contentsOf: split, at: left)
+                right+=split.count-1
+                blocks.swapAt(left, right)
+                break
+            }
+            left = 0
+            right -= 1
+        }
+
+        return blocks.flatten().enumerated().reduce(0) {
+            guard let val = $1.element else { return $0 }
+            return $0 + $1.offset * val
+        }
+
     }
 }
--- Tests/Day09.swift
+++ Tests/Day09.swift
@@ -11,8 +11,55 @@ struct Day09Tests {
     @Test func parsesCorrectly() async throws {
         let expected = [0, 0, nil, nil, nil, 1, 1, 1, nil, nil, nil, 2, nil, nil, nil, 3, 3, 3, nil, 4, 4, nil, 5, 5, 5, 5, nil, 6, 6, 6, 6, nil, 7, 7, 7, nil, 8, 8, 8, 8, 9, 9]
         let challenge = Day09(data: testData)
-        #expect(challenge.dataArray == expected)
+
+        for (expectedVal, challengeValue) in zip(expected, challenge.dataArray) {
+            #expect(challengeValue.size == 1)
+            guard let expectedVal else {
+                #expect(challengeValue is Empty)
+                continue
+            }
+
+            if let file = challengeValue as? File {
+                #expect(expectedVal == file.fileId)
+            } else {
+                Issue.record("Challenge Value was an Empty")
+            }
+
+        }
     }
+
+    @Test func mergesBlocksCorrectly() async throws {
+        let expected: [any DiskContent] = [
+            File(size: 2, fileId: 0),
+            Empty(size: 3),
+            File(size: 3, fileId: 1),
+            Empty(size: 3),
+            File(size: 1, fileId: 2),
+            Empty(size: 3),
+            File(size: 3, fileId: 3),
+            Empty(size: 1),
+            File(size: 2, fileId: 4),
+            Empty(size: 1),
+            File(size: 4, fileId: 5),
+            Empty(size: 1),
+            File(size: 4, fileId: 6),
+            Empty(size: 1),
+            File(size: 3, fileId: 7),
+            Empty(size: 1),
+            File(size: 4, fileId: 8),
+            File(size: 2, fileId: 9)
+        ]
+        let challenge = Day09(data: testData)
+        for (expectedVal, challengeValue) in zip(expected, challenge.mergeBlocks()) {
+            #expect(type(of: expectedVal) == type(of: challengeValue))
+            #expect(expectedVal.size == challengeValue.size)
+
+            guard let expectedFile = expectedVal as? File, let challengeFile = challengeValue as? File else { return }
+            #expect(expectedFile.fileId == challengeFile.fileId)
+        }
+
+    }
+
     @Test func testPart1() async throws {
         let challenge = Day09(data: testData)
         #expect(String(describing: challenge.part1()) == "1928")
@@ -20,6 +67,7 @@ struct Day09Tests {
 
     @Test func testPart2() async throws {
         let challenge = Day09(data: testData)
-        #expect(String(describing: challenge.part2()) == "0")
+
+        #expect(String(describing: challenge.part2()) == "2858")
     }
 }
